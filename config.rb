57acc1eb88018e402a09724050c5b9c945754793
Eric Koslow <ekoslow@gmail.com>
2011-04-29 22:38:30 -0400

    Moved files around into public directory

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@ require 'rubygems'
 require 'html5-boilerplate'
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "stylesheets"
+http_path = "/public"
+css_dir = "public/stylesheets"
 sass_dir = "src"
-images_dir = "images"
-javascripts_dir = "js"
+images_dir = "public/images"
+javascripts_dir = "public/js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
